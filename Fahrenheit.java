1c408bc30b4fa2c4d2a1eb65cc9c35968d0d9fe5
GabrieleBuenoA <gabrielebueno.ifsc@gmail.com>
2025-10-10 00:35:23 +0000

    Lista 5, num 1

---
+++ Fahrenheit.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Fahrenheit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    
+        System.out.println("Insira a temperatura Fahrenheit");
+        double numFahrenheit  = sc.nextDouble(); 
+
+        double numCelcius = (numFahrenheit - 32)*(5.0/9);
+
+        System.out.printf("O número, em Celcius, é %.3f %n", numCelcius);
+        
+        sc.close();
+    }
+}
